80e37928ea3523b0c2ada8410f0e7e334d2561b4
bmendanha <bmendanha@hotmail.com>
2020-02-04 14:47:18 +0000

    commit

---
+++ app2.js
@@ -0,0 +1,59 @@
+var myAlphabet=['A','B','C','D','E','F','G'];
+console.log(myAlphabet.length);
+
+function myAlphabetLength(){
+console.log(myAlphabet.length);
+}
+ myAlphabetLength();
+
+ var planets;
+ planets=['Mercurio','Venus','Terra','Marte','Jupter'];
+ console.log(planets[0]);
+ console.log(planets[1]);
+ console.log(planets[2]);
+ console.log(planets[3]);
+ console.log(planets[4]);
+
+ var wowDatatypes;
+ wowDatatypes=[12,"Bruno",12/06];
+ console.log(wowDatatypes);
+
+ var x=5,y=10,z=15;
+ console.log(x+y);
+ console.log(y-x);
+ console.log(x*y);
+ console.log(y/x);
+ console.log(x%2);
+ console.log(x++);
+ console.log(x--);
+
+var a=5, b=' Hello', c=' World', d=10;
+console.log(a+b);
+console.log(b+c);
+console.log(a+d);
+
+var a=5, b=10, c='5';
+var x=a;
+console.log(a==c);
+console.log(a===c);
+console.log(a==x);
+console.log(a !=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a>=c);
+console.log(a<=c);
+
+var hello=function(){
+return 'Hello World';
+}
+var result=hello();
+console.log(result);
+
+var hello2=function() {
+return 'Hi!';
+};
+var x=hello2();
+console.log(x);
+
